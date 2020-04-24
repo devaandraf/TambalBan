3947503ce20bedcc7d8f1a32916fd6724be6fbfd
devaandraf <andrafadila@gmail.com>
2020-04-24 15:29:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tambalban/Homepage.kt
@@ -0,0 +1,12 @@
+package com.example.tambalban
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Homepage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}
